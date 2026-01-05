f5f00f5de7b8ac13bcecabe755e21356b47e43f2
Kamil Matusiak <matusiak.kamil@outlook.com>
2026-01-05 12:27:06 +0100

    Render first image using PPM output Implements the initial image output stage based on "Ray Tracing in One Weekend".
    The program renders a simple RGB gradient and writes
    the result to standard output in PPM (P3) format,
    allowing flexible redirection to image files.
    
    This establishes the rendering pipeline and serves
    as the first functional milestone of the ray tracer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main() {
-    std::cout << "RayTracer\n";
+int main() 
+{
+    //Image
+    constexpr int image_width {256};
+    constexpr int image_height {256};
+
+    //Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int i = 0; i < image_height; i++)
+    {
+        for (int j = 0; j < image_width; j++)
+        {
+            auto r = static_cast<double>(j) / (image_width - 1);
+            auto g = static_cast<double>(i) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
     return 0;
 }
